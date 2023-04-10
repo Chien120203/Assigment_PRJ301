dfb126528c20233d19b4f44747674805ba23a5f4
Chien120203 <chiendam120203@gmail.com>
2023-04-10 16:12:55 +0700

    Update all

---
+++ src/java/entity/Student.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Đàm Quang Chiến
+ */
+public class Student {
+    private int id;
+    private String name, dob;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, String dob) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + ", dob=" + dob + '}';
+    }
+    
+    
+}
